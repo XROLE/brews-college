46a46225cf22385d0d5adce16e751548b9310881
xrole <xrolediamond@gmail.com>
2020-09-20 16:20:46 +0100

    add .sequelizerc file to configure sequelize install sequelize pakage setup database model and migration create user model and migration configure babel to transpile es6 code

--- server/index.js
+++ server/index.js
@@ -1,32 +1,9 @@
 import express from 'express';
-import dotenv from 'dotenv';
-import Sequelize from 'sequelize';
-
-
-dotenv.config();
-
-const sequelize = new Sequelize(
-    process.env.DB_NAME, 
-    process.env.DB_USER, 
-    process.env.DB_PASS, 
-    { host: process.env.DB_HOST, dialect: 'postgres' },
-);
-
-const initDatabase = async () => {
-    try{
-        await sequelize.authenticate();
-        console.log('I am a chosen one connected to postgress');
-    }catch(e) {
-        console.log('I am a chosen one Not connected to postgress yet =====> ', e)
-    };
-};
-
 
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    initDatabase();
     res.send('I am a chosen one');
 });
 
